e79d63e473ac3d209a99e639b89a5d7132f08b68
danilo.meneghel <danilo.meneghel@gmail.com>
2024-08-12 20:49:10 -0300

    Atualização de arquivos

--- src/main/java/pedido/controller/IndexController.java
+++ src/main/java/pedido/controller/IndexController.java
@@ -1,8 +1,11 @@
 package pedido.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@OpenAPIDefinition(info = @Info(title = "API REST", description = "Cadastro de Pedidos"))
 @Controller
 public class IndexController {
 
